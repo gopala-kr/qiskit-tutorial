f791177589d08c9874d822ab7e3139e622236c60
Gopala KR <gopal.rms4@gmail.com>
2018-03-04 22:57:51 +0530

    Update Qconfig.py

--- Qconfig.py
+++ Qconfig.py
@@ -1,4 +1,4 @@
-APItoken = '9a273b55fb6c782ed1a321b51e2849503a9f4d583ca2bf23dfff9f82618c532af439095ea55f8f9458e6b0ec64d09651dd61b24b924edd44f31d210b7013e1b0'
+APItoken = ' '
 config = {'url': 'https://quantumexperience.ng.bluemix.net/api'}
 
 if 'APItoken' not in locals():
