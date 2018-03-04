143539c0839d205a87057be0e6c8521aa1eb620b
Gopala KR <gopal.rms4@gmail.com>
2018-03-04 22:58:29 +0530

    Delete Qconfig.py

--- Qconfig.py
+++
@@ -1,5 +0,0 @@
-APItoken = ' '
-config = {'url': 'https://quantumexperience.ng.bluemix.net/api'}
-
-if 'APItoken' not in locals():
-    raise Exception('Please set up your access token. See Qconfig.py.')
